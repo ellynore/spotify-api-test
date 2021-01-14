9d91b0b82d58aa0ff7bb4c73b5dffc440d323c3f
Marco Ivarsson <marco.ivarsson@gmail.com>
2021-01-14 11:56:42 +0100

    Uncommented commented out code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions"
 
-// const fetch = require('node-fetch')
+const fetch = require('node-fetch')
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -10,40 +10,40 @@ export const helloWorld = functions.https.onRequest(
     functions.logger.info(request.query)
 
     let name = request.query.name ?? 'Firebase'
-    
+
     response.send(`Hello to ${name}!`)
   }
 )
 
-// // define what exists in the response
-// interface GetCurrentUserResponse {
-//   display_name: string
-//   id: string
-// }
+// define what exists in the response
+interface GetCurrentUserResponse {
+  display_name: string
+  id: string
+}
 
-// export const userDetails = functions.https.onRequest((request, response) => {
-//   functions.logger.info(request.query)
+export const userDetails = functions.https.onRequest((request, response) => {
+  functions.logger.info(request.query)
 
-//   let accessToken = request.query.token
+  let accessToken = request.query.token
 
-//   console.log(accessToken)
+  fetch('https://api.spotify.com/v1/me', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    }
+  })
+    .then((res: any) => {
+      functions.logger.info(res)
+      return res.json()
+    })
+    .then((json: GetCurrentUserResponse) => {
+      functions.logger.info("Spotify user:")
+      functions.logger.info(json["display_name"])
 
-//   response.send('FU')
-//   // fetch('https://api.spotify.com/v1/me', {
-//   //   method: 'GET',
-//   //   headers: {
-//   //     'Accept': 'application/json',
-//   //     'Content-Type': 'application/json',
-//   //     'Authorization': `Bearer ${accessToken}`
-//   //   }
-//   // })
-//   //   .then((res: any) => {
-//   //     functions.logger.info(res)
-//   //     return res.json()
-//   //   })
-//   //   .then((json: GetCurrentUserResponse) => {
-//   //     functions.logger.info("Spotify user:")
-//   //     functions.logger.info(json["display_name"])
-//   //   })
-
-// })
+      response.send({
+        displayName: json.display_name
+      })
+    })
+})
